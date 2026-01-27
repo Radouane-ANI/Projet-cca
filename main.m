e67f64e28b93dccc94e7222f75e6daae6c652f5f
Radouane Aouini <radouaneloo.aouini@gmail.com>
2026-01-27 20:47:19 +0100

    ajout test ilu

--- main.m
+++ main.m
@@ -2,7 +2,17 @@ load("data/494_bus.mat")
 
 M = Problem.A;
 
-[L,U] = lu(M);
-subplot(1,3,1); spy(M); title('Originale (M)');
-subplot(1,3,2); spy(L); title('Facteur L');
-subplot(1,3,3); spy(U); title('Facteur U');
+seuils = [1e-1, 1e-2, 1e-3, 1e-4];
+
+for s = seuils
+    res = test_ilu(M, s);
+    fprintf('Seuil: %.1e | Iter: %3d | Err. Rel: %.2e | Fill-in: %.2f | Flag: %d\n', ...
+        s, res.iterations, res.relative_error, res.fill_in, res.flag);
+    
+    % On trace la courbe de convergence
+    semilogy(res.resvec, 'DisplayName', ['Seuil ' num2str(s)]);
+    hold on;
+end
+legend;
+grid on;
+title('Influence de la précision du préconditionneur');
---
+++ test_ilu.m
@@ -0,0 +1,21 @@
+function [results] = test_ilu(A, droptol)    
+    n = size(A, 1);
+    b = A * ones(n, 1); 
+    
+    setup.type = 'ilutp';
+    setup.droptol = droptol;
+    [L, U] = ilu(A, setup);
+    
+    residual_matrix = A - (L * U);
+    results.error_fro = norm(residual_matrix, 'fro');
+    results.relative_error = results.error_fro / norm(A, 'fro');
+    results.fill_in = (nnz(L) + nnz(U)) / nnz(A);
+
+    tol = 1e-10; % Tolérance d'arrêt du solveur
+    maxit = 200;
+    [~, flag, ~, ~, resvec] = gmres(A, b, [], tol, maxit, L, U);
+    
+    results.resvec = resvec;
+    results.iterations = length(resvec) - 1;
+    results.flag = flag; % 0 = succès, 1 = échec
+end
